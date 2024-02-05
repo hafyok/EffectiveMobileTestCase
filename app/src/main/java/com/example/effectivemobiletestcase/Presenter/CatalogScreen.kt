302d1a7390cde58426cfa037e8f87f3f45c1712d
Haggai Khafizov <hafyok5777@gmail.com>
2024-02-05 22:08:05 +0300

    TopBar (Catalog, Popular filter, filter)

--- app/src/main/java/com/example/effectivemobiletestcase/Presenter/CatalogScreen.kt
+++ app/src/main/java/com/example/effectivemobiletestcase/Presenter/CatalogScreen.kt
@@ -1,11 +1,105 @@
 package com.example.effectivemobiletestcase.Presenter
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import com.example.effectivemobiletestcase.R
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.effectivemobiletestcase.ui.theme.black
+import com.example.effectivemobiletestcase.ui.theme.white
 
+
+@Preview
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CatalogScreen() {
-    Text(text = "Catalog")
+    Scaffold(
+        topBar = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Top
+            ){
+                Text(
+                    text = stringResource(R.string.catalog_title),
+                    style = TextStyle(fontSize = 16.sp,
+                        fontWeight = FontWeight(500)
+                    ),
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(vertical = 15.dp)
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(white)
+                    ) {
+                        Row {
+                            Icon(
+                                painter = painterResource(id = R.drawable.popul),
+                                contentDescription = "Favorite",
+                                tint = black
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(
+                                stringResource(id = R.string.popularity_filter),
+                                color = black,
+                                fontWeight = FontWeight(400)
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Icon(
+                                painter = painterResource(id = R.drawable.stroke_down),
+                                contentDescription = "Favorite",
+                                tint = black,
+                                modifier = Modifier.align(Alignment.CenterVertically)
+                            )
+                        }
+
+                    }
+                    Button(onClick = { }, colors = ButtonDefaults.buttonColors(white)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.filter),
+                            contentDescription = "Filter",
+                            tint = black,
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            stringResource(id = R.string.filter),
+                            color = black,
+                            fontWeight = FontWeight(400)
+                        )
+                    }
+                }
+            }
+        },
+        content = {  }
+    )
 }
+
+
+
